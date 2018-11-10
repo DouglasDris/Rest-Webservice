138ddf9cbdcaa23ab293df6d013ea0c04c8f0ef7
Douglas Dris <douglas.dris@sap.com>
2018-11-10 00:14:34 -0200

    Add a text just to know if the server is working as expected

--- src/com/srccodes/example/Wsg.java
+++ src/com/srccodes/example/Wsg.java
@@ -1,6 +1,8 @@
 package com.srccodes.example;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,8 +28,9 @@ public class Wsg extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+        PrintWriter printWriter  = response.getWriter();
+        printWriter.println("<h1>Warsong!</h1>");
 	}
 
 	/**
